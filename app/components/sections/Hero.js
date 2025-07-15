96d12e5aef0c86b09e7b340c5ac477be99054682
veeramadhan <95845583+veeramadhan@users.noreply.github.com>
2025-07-15 14:28:22 +0530

    resume.pdf added

--- app/components/sections/Hero.js
+++ app/components/sections/Hero.js
@@ -45,7 +45,11 @@ export default function Hero() {
 
         {/* Buttons */}
         <button className="mt-6 px-6 py-3 bg-gradient-to-r from-purple-500 to-indigo-600 hover:from-purple-600 hover:to-indigo-700 rounded-md shadow-md transition text-white font-semibold">
-          {t.download}
+          <a
+          href="/models/Veeramanikandan_CV_2025.pdf.pdf"
+          download
+          className= "bg-gradient-to-r from-purple-500 to-indigo-600 hover:from-purple-600 hover:to-indigo-700"
+        >{t.download}</a>
         </button>
 
     
